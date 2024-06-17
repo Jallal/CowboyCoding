f7eaa2b6eacf92734d1f99b423d05260a5eec58d
Jallal ☁ <jhazzat@gmail.com>
2024-06-17 14:53:10 +0000

    [Jallal] merge sort with list

---
+++ src/main/java/leetCode/dividAndConquer/ListNode.java
@@ -0,0 +1,22 @@
+package leetCode.dividAndConquer;
+import java.util.*;
+
+public class ListNode{
+
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+
+    }
+
+    public ListNode(int val) {
+         this.val = val;
+         }
+
+    public ListNode(int val, ListNode next) {
+         this.val = val; 
+         this.next = next; 
+        }
+
+}
